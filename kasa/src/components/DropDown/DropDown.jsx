c727877ad8f266801eeea2b3eaf778db44493644
Mede <dalemale09@gmail.com>
2024-06-22 03:50:56 +0200

    dropdown

--- kasa/src/components/DropDown/DropDown.jsx
+++ kasa/src/components/DropDown/DropDown.jsx
@@ -1,23 +1,25 @@
-
 import PropTypes from "prop-types";
+import "./drop.css";
 
 const DropDown = ({ title, content }) => {
     return (
-        <div style={{ width: '1023px', height: '54px' }} >
-        <details className="mt-6 mb-4 border   rounded-md">
-            <summary className="cursor-pointer py-2 px-4 bg-red-500 rounded-md text-lg text-white font-medium">
-                {title}
-            </summary>
-            <div className="px-4 py-2 bg-grey">
-                <p className="text-gray-700">
-                    {Array.isArray(content)
-                        ? content.map((item, index) => <span key={index} className="block mb-2">{item}</span>)
-                        : <span>{content}</span>}
-                </p>
-            </div>
-        </details></div>
+        <div className="dropdown-container">
+            <details className="dropdown">
+                <summary className="dropdown-summary">
+                    {title}
+                </summary>
+                <div className="dropdown-content">
+                    <p className="dropdown-text">
+                        {Array.isArray(content)
+                            ? content.map((item, index) => <span key={index} className="block mb-2">{item}</span>)
+                            : <span>{content}</span>}
+                    </p>
+                </div>
+            </details>
+        </div>
     );
-}; 
+};
+
 DropDown.propTypes = {
     title: PropTypes.string.isRequired,
     content: PropTypes.oneOfType([
@@ -25,4 +27,5 @@ DropDown.propTypes = {
         PropTypes.arrayOf(PropTypes.string)
     ]).isRequired,
 };
+
 export default DropDown;
--- kasa/src/components/footer/footer.jsx
+++ kasa/src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import logo from '../../asset/logo-white.svg';
 
 const Footer = () => {
     return (
-        <footer className="bg-black text-white py-4 text-center" style={{ width: '1440px', height: '209px' }}>
+        <footer className="bg-black text-white py-4 text-center mx-auto" style={{ width: '1440px', height: '209px' }}>
             <div className="flex flex-col items-center justify-center h-full">
                 <img src={logo} alt="kasa logo" className="w-122 h-39.44" />
                 <p className="text-sm mt-4">&copy; 2024 Kasa. All rights reserved</p>
