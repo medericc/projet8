e2d0aa2784072c310406231b1d4eba8a5b09a221
Mede <dalemale09@gmail.com>
2024-06-22 03:24:04 +0200

    css

--- kasa/src/components/footer/footer.jsx
+++ kasa/src/components/footer/footer.jsx
@@ -3,8 +3,10 @@ import logo from '../../asset/logo-white.svg';
 const Footer = () => {
     return (
         <footer className="bg-black text-white py-4 text-center" style={{ width: '1440px', height: '209px' }}>
-            <img src={logo} alt="kasa logo" className="w-122 h-39.44 mx-auto mb-4" />
-            <p className="text-sm">&copy; 2024 Kasa. All rights reserved</p>
+            <div className="flex flex-col items-center justify-center h-full">
+                <img src={logo} alt="kasa logo" className="w-122 h-39.44" />
+                <p className="text-sm mt-4">&copy; 2024 Kasa. All rights reserved</p>
+            </div>
         </footer>
     );
 }
